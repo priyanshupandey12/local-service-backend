9ceffb9f12e3b0e88e080bb32ae5b62f9fad7331
priyanshupandey12 <priyanshupandat32@gmail.com>
2026-03-08 15:26:08 +0530

    fixing

--- src/utils/generateToken.js
+++ src/utils/generateToken.js
@@ -1,17 +1,29 @@
-const jwt = require("jsonwebtoken");
+const jwt=require("jsonwebtoken");
 
 const generateToken = (res, user, message) => {
+  
   const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRES_IN,
+    expiresIn: process.env.JWT_EXPIRES_IN ,
   });
 
-  return res.status(200).json({
-    success: true,
-    message,
-    token,
-    userId: user._id,
-    role: user.role,
-  });
+
+
+  return res
+    .status(200)
+    .cookie("token", token, {
+   httpOnly: true,
+  secure: true,
+  sameSite: "none",
+  path:"/",
+      maxAge: 24 * 60 * 60 * 1000, 
+    })
+    .json({
+      success: true,
+      message,
+      userId: user._id,
+      role: user.role
+    });
 };
+
 
 module.exports = generateToken;
